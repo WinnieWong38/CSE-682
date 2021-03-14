8d0adb7c1a0a58b60228fa7d84588a7c395a55eb
MahaHas <maha.hassan2000@Gmail.com>
2021-03-14 11:59:15 -0700

    Update IIncomeService.java

--- src/main/java/com/example/CSE682/service/IIncomeService.java
+++ src/main/java/com/example/CSE682/service/IIncomeService.java
@@ -6,6 +6,28 @@ import com.example.CSE682.model.Income;
 
 public interface IIncomeService {
 	
+
+	Income save(Income income);
+	
+	Income edit(Income newIncome, Long id);
+	
+	void delete(Long id);
+	
+	Income getIncomeById(Long id);
+	
+	List<Income> getAllIncomes();
+
+
+}
+
+/*package com.example.CSE682.service;
+
+import java.util.List;
+
+import com.example.CSE682.model.Income;
+
+public interface IIncomeService {
+	
 	List<Income> getAll();
 	
 	Income save(Income income);
@@ -16,4 +38,4 @@ public interface IIncomeService {
 	
 	void delete(Long id);
 	
-}
+} */
