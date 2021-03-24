e2177c8afa6b7fd97506fddd47eee82cc4b59ff1
WinnieWong38 <winniew3875@gmail.com>
2021-03-24 10:43:34 -0400

    login and registration page styles
    Changed the styles for login, registration, and change password page. Changed icons. Added local date and isPaid value into constructors in HomeController

--- src/main/java/com/example/CSE682/controller/HomeController.java
+++ src/main/java/com/example/CSE682/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.CSE682.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,20 +32,19 @@ public class HomeController {
 //		Category b = new Category("Food");
 //		Category bSaved = categoryService.save(b);
 //		
-		
+//		
 //		Category g = new Category("Transportation");
 //		Category gSaved = categoryService.save(g);
 //		Category h = new Category("Entertainment");
 //		Category hSaved = categoryService.save(h);
-		
-		
-//		Expense c = new Expense("Apartment", aSaved, 1000);
+//				
+//		Expense c = new Expense("Apartment", aSaved, 1000.01, LocalDate.of(2020, 1, 8), true);
 //		Expense cSaved = expenseService.save(c);
-//		Expense d = new Expense("Groceries", bSaved, 200);
+//		Expense d = new Expense("Groceries", bSaved, 200.50, LocalDate.of(2021, 11, 18), false);
 //		Expense dSaved = expenseService.save(d);
-//		Expense e = new Expense("Summer Home", aSaved, 1500);
+//		Expense e = new Expense("Summer Home", aSaved, 1500, LocalDate.of(2020, 1, 8), true);
 //		Expense eSaved = expenseService.save(e);
-//		Expense f = new Expense("Chipotle", bSaved, 9.95);
+//		Expense f = new Expense("Chipotle", bSaved, 9.95, LocalDate.of(2020, 3, 21), true);
 //		Expense fSaved = expenseService.save(f);
 //		
 //		double total = expenseService.getTotalCost();
